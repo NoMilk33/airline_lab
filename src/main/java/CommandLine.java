f302a4a74d827b5e61fa7bed7e6a5ac824eb3759
Alhaadee <alhaadee91@gmail.com>
2022-08-30 16:38:09 +0100

    adds commandline class

--- src/main/java/CommandLine.java
+++ src/main/java/CommandLine.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class CommandLine {
+
+    private ArrayList<Flight> flightList;
+
+    private ArrayList<Passenger> passengerList;
+
+    public CommandLine() {
+        this.flightList = new ArrayList<>();
+        this.passengerList = new ArrayList<>();
+    }
+
+
+
+    public void addPassenger() {
+        return;
+
+    }
+
+    public String showFlights(){
+        return "empty";
+    }
+
+
 }
