c20007aa9ea39907de9ac5e38ffb5baa8c4c7a27
Guy Chalk <guychalk532@gmail.com>
2022-08-30 17:01:57 +0100

    guy's changes

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,7 +1,18 @@
+import java.util.Random;
+
 public class Passenger {
 
     String name;
     int contactNumber;
     int passengerID;
+
+    public Passenger(String name, int contactNumber){
+
+        this.name = name;
+        this.contactNumber = contactNumber;
+
+        Random rand = new Random();
+        this.passengerID = rand.nextInt(10000);
+    }
 
 }
