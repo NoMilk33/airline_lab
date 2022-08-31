dbc669d59c2498decf8bd871eae40bf21deea260
Kat Fagg <katjfagg@gmail.com>
2022-08-31 14:27:05 +0100

    completed MVP

--- src/test/java/CommandLineTest.java
+++ src/test/java/CommandLineTest.java
@@ -1,2 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CommandLineTest {
+
+    private CommandLine commandLine;
+
+    @BeforeEach
+    public void setUp(){
+        commandLine = new CommandLine();
+    }
+
+    @Test
+    public void canAddFlight(){
+        Flight testFlight = new Flight("Paris");
+        assertThat()
+    }
+
+
+
+
 }
