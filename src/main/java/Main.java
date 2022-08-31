3ff1bb23fd8f5fe5dd6b74003e2b0a1abaf21172
Kat Fagg <katjfagg@gmail.com>
2022-08-31 09:18:02 +0100

    merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("What would you like to do?");
+        System.out.println();
+    }
 
 
 }
